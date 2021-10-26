a414f4596d86cf60a87d89fd184436c0150b053c
Donovan Dicks <donovan.dicks@outlook.com>
2021-10-26 17:39:07 -0400

    Dashboard component is a wrapper around the major dashboard
    Subcomponents are included for the various tiles in the dashboard

---
+++ frontend/components/Dashboard.tsx
@@ -0,0 +1,44 @@
+import { Box, Container, Grid, Paper } from '@mui/material';
+import { experimentalStyled as styled } from '@mui/material/styles';
+import * as React from 'react';
+import TopTracks from './TopTracks';
+import TracksByGenre from './TracksByGenre';
+
+const Item = styled(Paper)(({ theme }) => ({
+  ...theme.typography.body2,
+  padding: theme.spacing(2),
+  textAlign: 'center',
+  color: theme.palette.text.secondary,
+}));
+
+export default function Dashboard() {
+  return (
+    <Box sx={{
+      backgroundColor: 'background.default',
+      minHeight: '100%'
+    }}>
+      <Container maxWidth={false}>
+        <Grid container spacing={3}>
+          <Grid
+            item
+            lg={8}
+            md={12}
+            xl={8}
+            xs={12}
+          >
+            <TopTracks />
+          </Grid>
+          <Grid
+            item
+            lg={4}
+            md={6}
+            xl={4}
+            xs={12}
+          >
+            <TracksByGenre />
+          </Grid>
+        </Grid>
+      </Container>
+    </Box>
+  );
+}
