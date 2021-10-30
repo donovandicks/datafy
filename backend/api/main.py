d84ced0398e3ac9c5a4bddb490cc2bc73d656888
Donovan Dicks <donovan.dicks@outlook.com>
2021-10-30 16:38:18 -0400

    Changed registry typing structure

--- backend/api/main.py
+++ backend/api/main.py
@@ -8,8 +8,8 @@ from resources.registry import RESOURCE_REGISTRY
 app = Flask(__name__)
 api = Api(app)
 
-for path, res in RESOURCE_REGISTRY.items():
-    api.add_resource(res, f"/{path}")
+for resource, path in RESOURCE_REGISTRY.items():
+    api.add_resource(resource, path)
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", debug=True)
--- backend/api/resources/registry.py
+++ backend/api/resources/registry.py
@@ -1,9 +1,11 @@
 """Resource Registry"""
 
+from flask_restful import Resource
+
 from resources.artists import Artists
 from resources.songs import Songs
 
-RESOURCE_REGISTRY = {
-    "artists": Artists,
-    "songs": Songs,
+RESOURCE_REGISTRY: dict[Resource, str] = {
+    Artists: "/artists",
+    Songs: "/songs",
 }
