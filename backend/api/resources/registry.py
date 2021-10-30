57513017fe870b1808aff40184b77dac49492087
Donovan Dicks <donovan.dicks@outlook.com>
2021-10-30 16:40:35 -0400

    Added documentation

--- backend/api/resources/registry.py
+++ backend/api/resources/registry.py
@@ -1,4 +1,11 @@
-"""Resource Registry"""
+"""Resource Registry
+
+Holds a registry of all resources and their associated paths. The registry is
+defined as a dict[Resource, str], where the str is the resource path. The
+resource registry is sourced from the main server program which registers the
+resources with the flask API instance. To register a new resource with the API,
+simply add the appropriate mapping to the resource registry.
+"""
 
 from flask_restful import Resource
 
