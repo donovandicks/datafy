f075d6fd9391d14b4eaec2edc3eb81915b3e00e1
Donovan Dicks <donovan.dicks@outlook.com>
2021-10-31 16:22:41 -0400

    Added the TableHeader component to the TopTracks component

--- frontend/components/TopTracks.tsx
+++ frontend/components/TopTracks.tsx
@@ -1,6 +1,21 @@
-import { Box, Card, CardHeader } from '@mui/material';
+import { Box, Card } from '@mui/material';
 import { DataGrid, GridColDef } from '@mui/x-data-grid';
 import { useEffect, useState } from 'react';
+import TableHeader from './TableHeader';
+
+const baseUrl = 'http://localhost:5000';
+
+const dateToTerm: { [key: string]: string } = {
+  'This month': 'short_term',
+  'Last 6 months': 'medium_term',
+  'Last 3 years': 'long_term',
+}
+
+interface SongRow {
+  id: number,
+  song: string,
+  artist: string,
+}
 
 const columns: GridColDef[] = [
   { field: 'id', headerName: 'Rank', width: 100 },
@@ -8,11 +23,41 @@ const columns: GridColDef[] = [
   { field: 'artist', headerName: 'Artists', width: 400 },
 ];
 
+const retrieveData = async (date: string) => {
+  const newRows = await fetch(`${baseUrl}/songs?time_range=${dateToTerm[date]}`, {
+    mode: 'cors'
+  })
+    .then(res => res.json())
+    .then((result) => {
+      return (result.map((res: any, idx: number) => ({
+        id: idx + 1,
+        song: res.song,
+        artist: res.artists.join(', '),
+      })));
+    });
+
+  return newRows;
+}
+
 const TopTracks = () => {
-  const [rows, setRows] = useState([]);
+  const [rows, setRows] = useState<SongRow[]>([]);
+  const [anchorEl, setAnchorEl] = useState(null);
+  const [selectedDate, setDate] = useState<string>('This month');
+
+  const handleClick = (event) => {
+    setAnchorEl(event.currentTarget);
+  }
+
+  const handleClose = async (date: string) => {
+    if (date !== selectedDate) {
+      setDate(date);
+      setRows(await retrieveData(date));
+    }
+    setAnchorEl(null);
+  }
 
   useEffect(() => {
-    fetch('http://localhost:5000/songs', {
+    fetch(`${baseUrl}/songs?time_range=short_term`, {
       mode: 'cors'
     })
       .then(res => res.json())
@@ -27,7 +72,13 @@ const TopTracks = () => {
 
   return (
     <Card>
-      <CardHeader title='Top Tracks' />
+      <TableHeader
+        title='Top Tracks'
+        selectedDate={selectedDate}
+        anchorEl={anchorEl}
+        handleClose={handleClose}
+        handleClick={handleClick}
+      />
       <Box sx={{ minWidth: 800 }}>
         <div style={{ height: 540, width: '100%' }}>
           <DataGrid
