f081ff0302122d837b87f5283dabb0d1fc871466
Donovan Dicks <donovan.dicks@outlook.com>
2021-10-31 22:07:06 -0400

    Added simple toolbar on dashboard

--- frontend/pages/index.tsx
+++ frontend/pages/index.tsx
@@ -1,9 +1,26 @@
-import Dashboard from "../components/Dashboard"
+import { Menu } from '@mui/icons-material/';
+import { AppBar, IconButton, Toolbar, Typography } from '@mui/material';
+import Dashboard from '../components/Dashboard';
 
-export default function App() {
+const App = () => {
   return (
     <div className="App">
-      <Dashboard></Dashboard>
+      <AppBar position="sticky">
+        <Toolbar variant="dense">
+          <IconButton edge="start" color="inherit" aria-label="menu">
+            <Menu />
+          </IconButton>
+          <Typography variant="h6" color="inherit">
+            Datafy
+          </Typography>
+        </Toolbar>
+      </AppBar>
+      {/* <Toolbar /> */}
+      <div className="dashboard">
+        <Dashboard></Dashboard>
+      </div>
     </div>
-  )
+  );
 }
+
+export default App;
