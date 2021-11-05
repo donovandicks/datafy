b81942145ffcdb2b0daf00a7b975a10c435f2b4f
Donovan Dicks <donovan.dicks@outlook.com>
2021-11-04 23:13:32 -0400

    Updated CLI to support genres resource

--- cli/main.py
+++ cli/main.py
@@ -1,22 +1,26 @@
 """The Datafy CLI"""
 
+import logging
 from argparse import ArgumentParser, Namespace
 from pprint import pprint
-from typing import Any, Type
+from typing import Any
 
 from prettytable import PrettyTable
-from requests import get
+from requests import RequestException, get
 
 TABLE_FIELDS = {
     "artists": ["Rank", "Artist"],
     "songs": ["Rank", "Song", "Artists"],
+    "genres": ["Genre", "Count"],
 }
 
+logging.basicConfig(level=logging.NOTSET)
+logger = logging.getLogger("cli_logger")
 
 class DatafyCLI:
     """The CLI Application for interacting with Spotify data from the terminal"""
 
-    def __init__(self) -> None:
+    def __init__(self, base_uri: str = "http://0.0.0.0:5000") -> None:
         """Initializes the CLI application with the ArgumentParser instance"""
 
         self.parser = ArgumentParser(description="""
@@ -25,19 +29,32 @@ A CLI application designed to interact with the Datafy backend from a terminal.
 
         self.args: Namespace = Namespace()
         self.table: PrettyTable = PrettyTable()
+        self.base_uri: str = base_uri
 
-    def add_argument(self, *name, arg_type: Type, choices: list[Any], arg_help: str):
+    def add_argument(
+        self,
+        *name: str,
+        arg_type: type,
+        choices: list[Any] = None,
+        arg_help: str,
+        req: bool = True,
+        default: Any = None,
+    ):
         """Add an argument to the CLI's argument parser
 
-        Args
-        ----
-        - *name [str|list[str]]: The name or tack-name of the argument, e.g. path or -p or --path
-        - arg_type [Type]: The primitive that the argument value should match and be coerced into
-        - choices [list[Any]]: The possible choices a user can make for the argument value
-        - arg_help [str]: The help message displayed in the terminal for this argument
+        Params
+        ------
+        *name: str|list[str]
+            the name or tack-name of the argument, e.g. path or -p or --path
+        arg_type: Type
+            the primitive that the argument value should match and be coerced into
+        choices: list[Any]
+            the possible choices a user can make for the argument value
+        arg_help: str
+            the help message displayed in the terminal for this argument
         """
         self.parser.add_argument(
-            *name, type=arg_type, choices=choices, help=arg_help, required=True
+            *name, type=arg_type, choices=choices, help=arg_help, required=req, default=default,
         )
         return self
 
@@ -53,29 +70,41 @@ A CLI application designed to interact with the Datafy backend from a terminal.
     def _parse_data(self, data: dict) -> list[list]:
         """Parses data according to the type of content being retrieved
 
-        Args
-        ----
-        - data [dict]: A dictionary of data retrieved from Spotify
+        Params
+        ------
+        data: dict
+            a dictionary of data retrieved from Spotify
 
         Returns
         -------
-        - [list[list]]: A list of data rows
+        parsed_data: list[list]
+            a list of data rows
         """
-        if self.args.content == "songs":
-            return [
-                [
-                    idx + 1, song["song"], ", ".join(song["artists"]),
-                ] for idx, song in enumerate(data)
-            ]
+        match self.args.content:
+            case "songs":
+                return [
+                    [
+                        idx + 1, song["song"], ", ".join(song["artists"]),
+                    ] for idx, song in enumerate(data["items"])
+                ]
 
-        if self.args.content == "artists":
-            return [
-                [
-                    idx + 1, artist,
-                ] for idx, artist in enumerate(data)
-            ]
+            case "artists":
+                return [
+                    [
+                        idx + 1, artist,
+                    ] for idx, artist in enumerate(data["items"])
+                ]
 
-        return [[]]
+            case "genres" if data:
+                return [
+                    [
+                        genre, count,
+                    ] for genre, count in data["items"].items()
+                ]
+
+            case _:
+                logging.warning("Unsupported content type")
+                return [[]]
 
     def display_data(self, data: list[list]):
         """Displays the data retrieved from Spotify in the terminal as a formatted table
@@ -89,19 +118,36 @@ A CLI application designed to interact with the Datafy backend from a terminal.
         self.table.add_rows(data)
         print(self.table)
 
+    def _send_request(self, endpoint):
+        response = get(endpoint)
+
+        match response.status_code:
+            case 200:
+                return response.json()
+
+            case _:
+                logger.exception("Failed to make request to Datafy: %s", response.text)
+                raise RequestException
+
     def run_command(self):
         """Execute the command determined by the arguments passed to the CLI"""
         self._parse_args()
 
-        response = get(f"http://0.0.0.0:5000/{self.args.content}?time_range={self.args.time_range}")
+        endpoint = ""
+        match self.args:
+            case Namespace(content="genres", time_range=str(rng), aggregate=bool(agg)):
+                endpoint = f"{self.base_uri}/genres?time_range={rng}&aggregate={agg}"
 
-        match response.status_code:
-            case 200:
-                parsed_data = self._parse_data(response.json())
-                self.display_data(parsed_data)
+            case Namespace(content=str(content), time_range=str(rng)):
+                endpoint = f"{self.base_uri}/{content}?time_range={rng}"
 
             case _:
-                print(f"Failed to make request to Datafy: {response.text}")
+                logger.exception("Unsupported CLI arguments passed %r", self.args)
+
+        data = self._send_request(endpoint)
+
+        parsed_data = self._parse_data(data)
+        self.display_data(parsed_data)
 
 
 if __name__ == "__main__":
@@ -109,7 +155,7 @@ if __name__ == "__main__":
         "-c",
         "--content",
         arg_type=str,
-        choices=["songs", "artists"],
+        choices=["songs", "artists", "genres"],
         arg_help="The type of content to retrieve, songs or artists",
     ).add_argument(
         "-t",
@@ -117,4 +163,11 @@ if __name__ == "__main__":
         arg_type=str,
         choices=["short_term", "medium_term", "long_term"],
         arg_help="The time period from which to retrieve data",
+    ).add_argument(
+        "-a",
+        "--aggregate",
+        arg_type=bool,
+        arg_help="Set to True to get an aggregated genre count",
+        req=False,
+        default=False,
     ).run_command()
