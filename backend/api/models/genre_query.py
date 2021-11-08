03eed2a08205cbaa1287cf39b0a33cf792c81057
Donovan Dicks <donovan.dicks@outlook.com>
2021-11-07 21:16:52 -0500

    Added an optional limit parameter to the genre query

--- backend/api/models/genre_query.py
+++ backend/api/models/genre_query.py
@@ -3,7 +3,6 @@
 from typing import Optional
 
 from pydantic import BaseModel  # pylint: disable=no-name-in-module
-from pydantic import validator
 
 from models.common import TimeRange
 
@@ -14,6 +13,7 @@ class GenreModel(BaseModel):
     content: Optional[str]
     time_range: Optional[TimeRange]
     aggregate: Optional[bool]
+    limit: Optional[int]
 
     # @validator("content")
     # def content_must_be_songs_or_artists(
