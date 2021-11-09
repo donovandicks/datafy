a491231c338c8db9d022cb10a2419fe1a0833482
Noah Kennedy <nak@DESKTOP-DINQGHV.home>
2021-11-09 02:41:48 -0500

    created rec query and reponse models

---
+++ backend/api/models/recommendation_query.py
@@ -0,0 +1,20 @@
+"""Query models for Spotify recommendations"""
+
+from typing import Optional
+
+from pydantic import BaseModel  # pylint: disable=no-name-in-module
+
+
+class RecommendationQuery(BaseModel):
+    """The query model for the Recommendations resource"""
+
+    # just the required fields for now, want to add in things like target sounds and whatnot
+    seed_artists: Optional[str]
+    seed_genres: Optional[str]
+    seed_tracks: Optional[str]
+
+    class Config:
+        """Defines the configuration for the query model"""
+
+        # i don't think this line is necessary for the rec model
+        # use_enum_values = True
