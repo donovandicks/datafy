c764d530eee4f05704ae9a5a03aaa15f299f574b
Noah Kennedy <nak@DESKTOP-DINQGHV.home>
2021-11-09 17:54:22 -0500

    added comment w/ example rec queries

--- backend/api/models/recommendation_query.py
+++ backend/api/models/recommendation_query.py
@@ -15,6 +15,24 @@ class RecommendationQuery(BaseModel):
     seed_tracks: Optional[str]
     limit: Optional[int]
 
+    """
+    example queries for testing use:
+
+    - Recommendations for seed artists of Trav and Ye with a limit of 5
+    curl http://localhost:5000/recommendations\?seed_artists\=5K4W6rqBFWDnAN6FQUkS6x,0Y5tJX1MQlPlqiwlOH1tJY\&limit\=5
+    - just trav
+    curl http://localhost:5000/recommendations\?seed_artists\=0Y5tJX1MQlPlqiwlOH1tJY\&limit\=5
+    - just ye
+    curl http://localhost:5000/recommendations\?seed_artists\=5K4W6rqBFWDnAN6FQUkS6x\&limit\=5
+
+    - seed track of Orange Soda (he's baby keem)
+    curl http://localhost:5000/recommendations\?seed_tracks\=5FkoSXiJPKTNyYgALRJFhD\&limit\=5
+
+    - seed tracks of Fade and Waves
+    curl http://localhost:5000/recommendations\?seed_tracks\=3cCxoOgfi6hgt8MNteuiiD,3nAq2hCr1oWsIU54tS98pL\&limit\=5
+
+    """
+
     class Config:
         """Defines the configuration for the query model"""
 
