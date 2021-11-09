271bf3a3467d97abf60e058c130b629f45ea212f
Donovan Dicks <donovan.dicks@outlook.com>
2021-11-09 01:07:23 -0500

    Nit: words in documentation

--- backend/api/models/artist_response.py
+++ backend/api/models/artist_response.py
@@ -1,9 +1,9 @@
-"""Defines the structure of the data returned by the /artists response"""
+"""Defines the structure of the data returned by the artists response"""
 
 from pydantic import BaseModel
 
 
 class ArtistResponse(BaseModel):
-    """The object model of the response body for the /artists endpoint"""
+    """The object model of the response body for the artists endpoint"""
 
     items: list[str]
