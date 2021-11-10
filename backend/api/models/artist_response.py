444e53bbe63085969e77d3cc5f1e1f0171671953
Donovan <45213132+donovandicks@users.noreply.github.com>
2021-11-09 23:14:53 -0500

    Updated artists resource to return more data (#64)
    Now includes the artist ID, name, popularity, and followers

--- backend/api/models/artist_response.py
+++ backend/api/models/artist_response.py
@@ -3,7 +3,39 @@
 from pydantic import BaseModel
 
 
-class ArtistResponse(BaseModel):
-    """The object model of the response body for the artists endpoint"""
+class Artist(BaseModel):
+    """
+    The object model representing key elements of a Spotify artist
+    """
 
-    items: list[str]
+    id: str
+    """The Spotify ID of the artist"""
+
+    name: str
+    """The name of the artist"""
+
+    popularity: int
+    "The popularity of the artist from 0 to 100"
+
+    followers: int
+    """The total number of followers that the artist has"""
+
+
+class ArtistResponse(BaseModel):
+    """
+    The object model of the response body for the artists endpoint
+
+    ```
+    items: [
+        {
+            id: string,
+            name: string,
+            popularity: int,
+            followers: int,
+        }
+    ]
+    ```
+    """
+
+    items: list[Artist]
+    """The list of all Artists retrieved from Spotify"""
