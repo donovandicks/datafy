349da7ae00ba53d76d924be7238595f7c7bca11e
Noah Kennedy <nak@DESKTOP-DINQGHV.home>
2021-11-09 22:35:34 -0500

    added comment

--- backend/api/resources/recommendations.py
+++ backend/api/resources/recommendations.py
@@ -24,6 +24,8 @@ class Recommendations(Resource, BaseService):
         super().__init__()
 
     def __format_seed_lists(self) -> None:
+        """Turns Strings of comma separated values into lists of Strings"""
+
         self.query.seed_artists = (
             self.query.seed_artists.split(",")
             if self.query.seed_artists
