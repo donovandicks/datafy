b26e0772d105f957315a3337a1d1a6fbf89c30aa
Donovan Dicks <donovan.dicks@outlook.com>
2021-11-14 17:21:30 -0500

    Moved retrieve and display functions to content

--- rcli/src/models/content.rs
+++ rcli/src/models/content.rs
@@ -1,4 +1,6 @@
+use crate::libs::url_builder::URLBuilder;
 use prettytable::Row;
+use serde::de::DeserializeOwned;
 
 pub trait Content {
     fn as_row(&self, idx: &usize) -> Row;
@@ -6,4 +8,21 @@ pub trait Content {
 
 pub trait ContentCollection {
     fn display(&self);
+}
+
+async fn retrieve_content<T>(endpoint: &str) -> Result<T, Box<dyn std::error::Error>>
+where
+    T: ContentCollection + DeserializeOwned,
+{
+    let resp = reqwest::get(endpoint).await?.json::<T>().await?;
+    Ok(resp)
+}
+
+pub async fn display_content<T>(resource: &str)
+where
+    T: ContentCollection + DeserializeOwned,
+{
+    let endpoint = URLBuilder::new().with_resource(resource).build();
+    let content: T = retrieve_content(&endpoint).await.unwrap();
+    content.display();
 }
