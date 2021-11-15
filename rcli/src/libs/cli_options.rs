9549e6bc0216d73c2052c7d2b8cba8ee50845985
Donovan Dicks <donovan.dicks@outlook.com>
2021-11-14 22:57:26 -0500

    Added support for all other API endpoints

--- rcli/src/libs/cli_options.rs
+++ rcli/src/libs/cli_options.rs
@@ -1,7 +1,7 @@
 use crate::libs::url_builder::URLBuilder;
-use crate::models::collections::ArtistCollection;
+use crate::models::collections::{ArtistCollection, GenreCollection, SongCollection};
 use crate::models::content::{retrieve_content, ContentCollection};
-use dialoguer::{theme::ColorfulTheme, Input};
+use dialoguer::{theme::ColorfulTheme, Input, Select};
 
 enum CLIOptions {
     ArtistOptions {
@@ -19,8 +19,8 @@ enum CLIOptions {
     },
 }
 
-fn choose_artist_options() -> CLIOptions {
-    let limit: String = Input::with_theme(&ColorfulTheme::default())
+fn get_limit() -> String {
+    Input::with_theme(&ColorfulTheme::default())
         .with_prompt("Results Limit: ")
         .default(20.to_string())
         .validate_with(|input: &String| -> Result<(), &str> {
@@ -30,9 +30,11 @@ fn choose_artist_options() -> CLIOptions {
             }
         })
         .interact_text()
-        .unwrap();
+        .unwrap()
+}
 
-    let time_range: String = Input::with_theme(&ColorfulTheme::default())
+fn get_time_range() -> String {
+    Input::with_theme(&ColorfulTheme::default())
         .with_prompt("Time Range: ")
         .default(String::from("medium_term"))
         .validate_with(|input: &String| -> Result<(), &str> {
@@ -42,30 +44,63 @@ fn choose_artist_options() -> CLIOptions {
             }
         })
         .interact_text()
-        .unwrap();
+        .unwrap()
+}
 
+fn get_aggregate() -> String {
+    let choices = ["true", "false"];
+    let index = match Select::with_theme(&ColorfulTheme::default())
+        .with_prompt("Aggregate Results: ")
+        .items(&choices)
+        .default(1)
+        .interact()
+    {
+        Ok(index) => index,
+        _ => panic!("Somehow received a value out of selection!"),
+    };
+
+    String::from(choices[index])
+}
+
+fn choose_artist_options() -> CLIOptions {
     CLIOptions::ArtistOptions {
-        limit: limit,
-        time_range: time_range,
+        limit: get_limit(),
+        time_range: get_time_range(),
+    }
+}
+
+fn choose_song_options() -> CLIOptions {
+    CLIOptions::SongOptions {
+        limit: get_limit(),
+        time_range: get_time_range(),
+    }
+}
+
+fn choose_genre_options() -> CLIOptions {
+    CLIOptions::GenreOptions {
+        limit: get_limit(),
+        time_range: get_time_range(),
+        aggregate: get_aggregate(),
     }
 }
 
 pub async fn fetch_content(resource: &str) {
-    let resourse_lower = resource.to_lowercase();
-    let opts = match resourse_lower.as_ref() {
+    let resource_lower = resource.to_lowercase();
+    let opts = match resource_lower.as_ref() {
         "artists" => choose_artist_options(),
+        "genres" => choose_genre_options(),
+        "songs" => choose_song_options(),
         _ => unimplemented!(),
     };
 
     match opts {
         CLIOptions::ArtistOptions { limit, time_range } => {
             let url = URLBuilder::new()
-                .with_resource(resourse_lower.as_ref())
+                .with_resource(resource_lower.as_ref())
                 .with_param("limit", limit)
                 .with_param("time_range", time_range.to_lowercase())
                 .build();
 
-            println!("URL: {}", url);
             retrieve_content::<ArtistCollection>(&url)
                 .await
                 .unwrap()
@@ -75,7 +110,30 @@ pub async fn fetch_content(resource: &str) {
             limit,
             time_range,
             aggregate,
-        } => {}
-        CLIOptions::SongOptions { limit, time_range } => {}
+        } => {
+            let url = URLBuilder::new()
+                .with_resource(resource_lower.as_ref())
+                .with_param("limit", limit)
+                .with_param("time_range", time_range)
+                .with_param("aggregate", aggregate)
+                .build();
+
+            retrieve_content::<GenreCollection>(&url)
+                .await
+                .unwrap()
+                .display();
+        }
+        CLIOptions::SongOptions { limit, time_range } => {
+            let url = URLBuilder::new()
+                .with_resource(resource_lower.as_ref())
+                .with_param("limit", limit)
+                .with_param("time_range", time_range)
+                .build();
+
+            retrieve_content::<SongCollection>(&url)
+                .await
+                .unwrap()
+                .display();
+        }
     }
 }
