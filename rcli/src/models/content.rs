91dc6d5db330a638e7230fe23701ddf5d15b28fa
Donovan Dicks <donovan.dicks@outlook.com>
2021-11-14 20:07:52 -0500

    Refactored by removing some methods

--- rcli/src/models/content.rs
+++ rcli/src/models/content.rs
@@ -1,4 +1,3 @@
-use crate::libs::url_builder::URLBuilder;
 use prettytable::Row;
 use serde::de::DeserializeOwned;
 
@@ -10,19 +9,10 @@ pub trait ContentCollection {
     fn display(&self);
 }
 
-async fn retrieve_content<T>(endpoint: &str) -> Result<T, Box<dyn std::error::Error>>
+pub async fn retrieve_content<T>(endpoint: &str) -> Result<T, Box<dyn std::error::Error>>
 where
     T: ContentCollection + DeserializeOwned,
 {
     let resp = reqwest::get(endpoint).await?.json::<T>().await?;
     Ok(resp)
-}
-
-pub async fn display_content<T>(resource: &str)
-where
-    T: ContentCollection + DeserializeOwned,
-{
-    let endpoint = URLBuilder::new().with_resource(resource).build();
-    let content: T = retrieve_content(&endpoint).await.unwrap();
-    content.display();
 }
