80b97a07f0b61de4362926f91f518b43061c9d90
Donovan Dicks <donovan.dicks@outlook.com>
2021-11-16 00:48:50 -0500

    Heavy overhaul of the processor functions

--- rcli/src/libs/processors.rs
+++ rcli/src/libs/processors.rs
@@ -10,55 +10,23 @@ use crate::models::{
     content::{retrieve_content, ContentCollection},
 };
 use serde::de::DeserializeOwned;
+use std::boxed::Box;
 
-async fn display_content<T>(url: &str)
+async fn fetch_content<T>(url: &str) -> T
 where
     T: ContentCollection + DeserializeOwned,
 {
-    retrieve_content::<T>(url).await.unwrap().display();
+    retrieve_content::<T>(url).await.unwrap()
 }
 
-async fn fetch_artists(builder: &mut URLBuilder, limit: &str, time_range: &str) {
-    let url = builder
-        .with_param("limit", limit)
-        .with_param("time_range", time_range)
-        .build();
-
-    display_content::<ArtistCollection>(&url).await;
-}
-
-async fn fetch_genres(builder: &mut URLBuilder, limit: &str, time_range: &str, aggregate: &str) {
-    let url = builder
-        .with_param("limit", limit)
-        .with_param("time_range", time_range)
-        .with_param("aggregate", aggregate)
-        .build();
-
-    display_content::<GenreCollection>(&url).await;
-}
-
-async fn fetch_songs(builder: &mut URLBuilder, limit: &str, time_range: &str) {
-    let url = builder
-        .with_param("limit", limit)
-        .with_param("time_range", time_range)
-        .build();
-
-    display_content::<SongCollection>(&url).await;
-}
-
-async fn fetch_recs(
-    builder: &mut URLBuilder,
-    seed_artists: &str,
-    seed_genres: &str,
-    seed_tracks: &str,
-) {
-    let url = builder
-        .with_param("seed_artists", seed_artists)
-        .with_param("seed_genres", seed_genres)
-        .with_param("seed_tracks", seed_tracks)
-        .build();
-
-    display_content::<RecommendationCollection>(&url).await;
+fn get_user_selections(resource: &str) -> CLIOptions {
+    match resource {
+        "artists" => choose_artist_options(),
+        "genres" => choose_genre_options(),
+        "recommendations" => choose_rec_options(),
+        "songs" => choose_song_options(),
+        _ => unimplemented!(),
+    }
 }
 
 /// Fetches the appropriate content for the supplied resource and displays the
@@ -67,44 +35,71 @@ async fn fetch_recs(
 /// # Args
 ///
 /// * `resource` - A str reference for the API resource from which to get content
-pub async fn fetch_content(resource: &str) {
+pub async fn process_request(resource: &str) {
     let resource_lower = resource.to_lowercase();
-    let opts = match resource_lower.as_ref() {
-        "artists" => choose_artist_options(),
-        "genres" => choose_genre_options(),
-        "recommendations" => choose_rec_options(),
-        "songs" => choose_song_options(),
-        _ => unimplemented!(),
-    };
+    let opts = get_user_selections(&resource_lower);
 
     // Temp var raw_builder necessary to avoid E0716 where the temp value is dropped
     // before it is referenced later
     let mut raw_builder: URLBuilder = URLBuilder::new();
     let builder = raw_builder.with_resource(&resource_lower);
 
-    match opts {
-        CLIOptions::Artist { limit, time_range } => {
-            fetch_artists(builder, &limit, &time_range.to_lowercase()).await;
-        }
+    let content: Box<dyn ContentCollection> = match opts {
+        CLIOptions::Artist { limit, time_range } => Box::new(
+            fetch_content::<ArtistCollection>(
+                &builder
+                    .with_params(vec![("limit", &limit), ("time_range", &time_range)])
+                    .build(),
+            )
+            .await,
+        ),
 
         CLIOptions::Genre {
             limit,
             time_range,
             aggregate,
-        } => {
-            fetch_genres(builder, &limit, &time_range.to_lowercase(), &aggregate).await;
-        }
+        } => Box::new(
+            fetch_content::<GenreCollection>(
+                &builder
+                    .with_params(vec![
+                        ("limit", &limit),
+                        ("time_range", &time_range.to_lowercase()),
+                        ("aggregate", &aggregate),
+                    ])
+                    .build(),
+            )
+            .await,
+        ),
 
-        CLIOptions::Song { limit, time_range } => {
-            fetch_songs(builder, &limit, &time_range.to_lowercase()).await;
-        }
+        CLIOptions::Song { limit, time_range } => Box::new(
+            fetch_content::<SongCollection>(
+                &builder
+                    .with_params(vec![
+                        ("limit", &limit),
+                        ("time_range", &time_range.to_lowercase()),
+                    ])
+                    .build(),
+            )
+            .await,
+        ),
 
         CLIOptions::Recommendation {
             seed_artists,
             seed_genres,
             seed_tracks,
-        } => {
-            fetch_recs(builder, &seed_artists, &seed_genres, &seed_tracks).await;
-        }
-    }
+        } => Box::new(
+            fetch_content::<RecommendationCollection>(
+                &builder
+                    .with_params(vec![
+                        ("seed_artists", &seed_artists),
+                        ("seed_genres", &seed_genres),
+                        ("seed_tracks", &seed_tracks),
+                    ])
+                    .build(),
+            )
+            .await,
+        ),
+    };
+
+    content.display();
 }
