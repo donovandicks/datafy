6a2385f07d887cd19ecf15c4a5fc77e8a9bcbee4
Donovan Dicks <donovan.dicks@outlook.com>
2021-11-26 18:58:23 -0500

    Run uvicorn programmatically

--- backend/fapi/main.py
+++ backend/fapi/main.py
@@ -1,5 +1,6 @@
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
+from uvicorn import run
 
 from routers import artists
 
@@ -8,3 +9,6 @@ app.add_middleware(
     CORSMiddleware,
 )
 app.include_router(artists.router)
+
+if __name__ == "__main__":
+    run("main:app", host="0.0.0.0", port=8000, log_level="debug")
