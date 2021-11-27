43f49faf5a445bd908dcc8ad044adba4d17f324b
Donovan Dicks <donovan.dicks@outlook.com>
2021-11-26 19:31:08 -0500

    Added songs route

--- backend/fapi/main.py
+++ backend/fapi/main.py
@@ -1,14 +1,17 @@
+"""Main server driver"""
+
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
 from uvicorn import run
 
-from routers import artists
+from routers import artists, songs
 
 app = FastAPI()
 app.add_middleware(
     CORSMiddleware,
 )
 app.include_router(artists.router)
+app.include_router(songs.router)
 
 if __name__ == "__main__":
-    run("main:app", host="0.0.0.0", port=8000, log_level="debug")
+    run("main:app", host="0.0.0.0", port=8000, log_level="debug", reload=True)
