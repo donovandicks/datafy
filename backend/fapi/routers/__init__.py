2a2943626c45160e4a72a7f04decd985bca2bc76
Donovan Dicks <donovan.dicks@outlook.com>
2021-11-27 23:06:46 -0500

    Adds the recs router

--- backend/fapi/routers/__init__.py
+++ backend/fapi/routers/__init__.py
@@ -4,11 +4,13 @@ from typing import Dict, Type
 from fastapi import APIRouter
 
 from .artists import router as artists_router
-from .genres import router as genre_router
+from .genres import router as genres_router
+from .recs import router as recs_router
 from .songs import router as songs_router
 
 ROUTE_REGISTRY: list[APIRouter] = [
     artists_router,
-    genre_router,
+    genres_router,
+    recs_router,
     songs_router,
 ]
