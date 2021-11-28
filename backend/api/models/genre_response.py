b6664b92b1f690b6c85855ad56c9626eba2a84cc
Donovan Dicks <donovan.dicks@outlook.com>
2021-11-28 00:42:10 -0500

    Removed old api

--- backend/api/models/genre_response.py
+++
@@ -1,9 +0,0 @@
-"""Defines the structure of the data returned by the genres response"""
-
-from pydantic import BaseModel
-
-
-class GenreResponse(BaseModel):
-    """The object model of the response body for the genres endpoint"""
-
-    items: dict[str, int]
