788db6d811f4a48b6cb0298addfc9841a2f76d7d
Donovan Dicks <donovandicks@donovans-mbp.lan>
2021-12-04 02:43:21 -0500

    Update tests

--- backend/api/tests/test_genres.py
+++ backend/api/tests/test_genres.py
@@ -1,7 +1,8 @@
 """Test Suite for the `/genres` route"""
 from unittest import TestCase
 
-from models.genre import Genre, GenreCollection, GenreQuery
+from models.collection import Collection
+from models.genre import Genre, GenreQuery
 from routers import genres
 
 from .client_fixture import FakeClient
@@ -13,22 +14,24 @@ class GenresTest(TestCase):
     def test_get_genre_agg(self):
         """Tests get_genres with aggregation"""
         self.assertEqual(
-            GenreCollection(
+            Collection(
+                item_type="Genre",
                 items=[
-                    Genre(name="hip hop", count=10),
-                    Genre(name="rap", count=7),
-                    Genre(name="pop", count=5),
-                    Genre(name="r&b", count=0),
-                    Genre(name="metal", count=0),
-                    Genre(name="rock", count=0),
-                    Genre(name="indie", count=0),
-                    Genre(name="soul", count=0),
-                    Genre(name="folk", count=0),
-                    Genre(name="electronic", count=0),
-                    Genre(name="country", count=0),
-                    Genre(name="jazz", count=0),
-                    Genre(name="classical", count=0),
+                    Genre(content="Genre", name="hip hop", count=10),
+                    Genre(content="Genre", name="rap", count=7),
+                    Genre(content="Genre", name="pop", count=5),
+                    Genre(content="Genre", name="r&b", count=0),
+                    Genre(content="Genre", name="metal", count=0),
+                    Genre(content="Genre", name="rock", count=0),
+                    Genre(content="Genre", name="indie", count=0),
+                    Genre(content="Genre", name="soul", count=0),
+                    Genre(content="Genre", name="folk", count=0),
+                    Genre(content="Genre", name="electronic", count=0),
+                    Genre(content="Genre", name="country", count=0),
+                    Genre(content="Genre", name="jazz", count=0),
+                    Genre(content="Genre", name="classical", count=0),
                 ],
+                item_headers=["Rank", "Genre", "Count"],
                 count=13,
             ),
             genres.get_genres(FakeClient(GenreQuery(aggregate=True))),
@@ -37,15 +40,17 @@ class GenresTest(TestCase):
     def test_get_genre_detail(self):
         """Tests get_genres without aggregation"""
         self.assertEqual(
-            GenreCollection(
+            Collection(
+                item_type="Genre",
                 items=[
-                    Genre(name="rap", count=5),
-                    Genre(name="hip hop", count=5),
-                    Genre(name="underground hip hop", count=3),
-                    Genre(name="pop", count=3),
-                    Genre(name="alternative hip hop", count=2),
-                    Genre(name="pop rap", count=2),
+                    Genre(content="Genre", name="rap", count=5),
+                    Genre(content="Genre", name="hip hop", count=5),
+                    Genre(content="Genre", name="underground hip hop", count=3),
+                    Genre(content="Genre", name="pop", count=3),
+                    Genre(content="Genre", name="alternative hip hop", count=2),
+                    Genre(content="Genre", name="pop rap", count=2),
                 ],
+                item_headers=["Rank", "Genre", "Count"],
                 count=6,
             ),
             genres.get_genres(FakeClient(GenreQuery(aggregate=False))),
