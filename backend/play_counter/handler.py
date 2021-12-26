d0ece9e03cefe73dc6213dfd0c7c18c8fb29a810
Donovan Dicks <donovan.dicks@outlook.com>
2021-12-26 13:16:29 -0500

    Setup project as serverless

---
+++ backend/play_counter/handler.py
@@ -0,0 +1,30 @@
+"""Main entry point for the play-counter lambda"""
+
+import datetime
+import logging
+
+logger = logging.getLogger(__name__)
+logger.setLevel(logging.INFO)
+
+
+def log_start(func_name: str):
+    """
+    Logs the execution of a lambda function
+
+    func_name: str
+        the name of the lambda function
+    """
+    current_time = datetime.datetime.now().time()
+    logger.info("Function %s ran at %s", func_name, str(current_time))
+
+
+def run(_, context):
+    """
+    The lambda handler called on function execution
+
+    _:
+        the event received on function execution
+    context:
+        the lambda function metadata
+    """
+    log_start(context.function_name)
