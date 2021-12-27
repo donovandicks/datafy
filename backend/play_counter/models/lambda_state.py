2eae68f2c98d790915fe9d2e2437113c8a936a24
Donovan Dicks <donovan.dicks@outlook.com>
2021-12-27 17:20:32 -0500

    Enum to represent lambda state

---
+++ backend/play_counter/models/lambda_state.py
@@ -0,0 +1,11 @@
+from enum import Enum
+
+
+class LambdaAction(Enum):
+    """
+    States representing pieces of the lambda function lifecycle
+    """
+
+    TRIGGERED = 1
+    COMPLETED = 2
+    FAILED = 3
