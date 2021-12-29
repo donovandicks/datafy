bb1866bc9e985d3edb4b2e937fa06b7c7c414e8d
Donovan Dicks <donovan.dicks@outlook.com>
2021-12-28 22:11:49 -0500

    Update handler to dynamically schedule itself
    Updates to slow down trigger rate if the user is not listening to a song

--- backend/play_counter/handler.py
+++ backend/play_counter/handler.py
@@ -1,7 +1,10 @@
 """Main entry point for the play-counter lambda"""
+from os import environ
 
+from botocore.exceptions import ClientError
 from structlog import get_logger
 
+from clients.aws import AWS
 from clients.spotify import SpotifyClient
 from models.lambda_state import LambdaAction
 from telemetry.logging import Logger
@@ -20,11 +23,22 @@ def run(_, context):
         the lambda function metadata
     """
     logger.log_execution(context.function_name, LambdaAction.TRIGGERED)
+    eb_rule = environ["EVENT_BRIDGE_RULE"]
+    aws_client = AWS()
+    sp_client = SpotifyClient(aws_client=aws_client)
 
     try:
-        sp_client = SpotifyClient()
-        sp_client.get_current_song()
-    except Exception as ex:
+        current_rule_schedule = aws_client.get_rule_schedule(name=eb_rule)
+        current_song = sp_client.get_current_song()
+
+        if not current_song and (current_rule_schedule != "rate(5 minutes)"):
+            aws_client.update_event_rule(name=eb_rule, rate=5)
+            return
+
+        if current_song and (current_rule_schedule == "rate(5 minutes)"):
+            aws_client.update_event_rule(name=eb_rule, rate=1)
+
+    except ClientError as ex:
         logger.log_failure(context.function_name, ex)
 
     logger.log_execution(context.function_name, LambdaAction.COMPLETED)
