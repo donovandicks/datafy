29c04068c8ce9cfcd55aabf7411b95cc764b88f7
Donovan Dicks <donovan.dicks@outlook.com>
2021-12-29 21:12:23 -0500

    Abstract scheduling

--- backend/play_counter/handler.py
+++ backend/play_counter/handler.py
@@ -1,4 +1,5 @@
 """Main entry point for the play-counter lambda"""
+from datetime import datetime, time
 from os import environ
 
 from botocore.exceptions import ClientError
@@ -13,6 +14,34 @@ log_client = get_logger(__name__)
 logger = Logger(logger=log_client)
 
 
+def reschedule(aws_client: AWS, listening_now: bool):
+    """
+    Reschedules the EventBridge rule responsible for triggering this lambda
+
+    Params
+    ------
+    aws_client: AWS
+    listening_now: bool
+    """
+    eb_rule = environ["EVENT_BRIDGE_RULE"]
+    current_rule_schedule = aws_client.get_rule_schedule(name=eb_rule)
+
+    if (
+        time.fromisoformat("02:00:00")
+        < datetime.now().time()
+        < time.fromisoformat("08:00:00")
+    ) and (current_rule_schedule != "rate(1 hour)"):
+        aws_client.update_event_rule(name=eb_rule, rate=1, period="hour")
+        return
+
+    if not listening_now and (current_rule_schedule != "rate(5 minutes)"):
+        aws_client.update_event_rule(name=eb_rule, rate=5, period="minutes")
+        return
+
+    if listening_now and (current_rule_schedule == "rate(5 minutes)"):
+        aws_client.update_event_rule(name=eb_rule, rate=1, period="minute")
+
+
 def run(_, context):
     """
     The lambda handler called on function execution
@@ -23,21 +52,12 @@ def run(_, context):
         the lambda function metadata
     """
     logger.log_execution(context.function_name, LambdaAction.TRIGGERED)
-    eb_rule = environ["EVENT_BRIDGE_RULE"]
     aws_client = AWS()
     sp_client = SpotifyClient(aws_client=aws_client)
 
     try:
-        current_rule_schedule = aws_client.get_rule_schedule(name=eb_rule)
         current_song = sp_client.get_current_song()
-
-        if not current_song and (current_rule_schedule != "rate(5 minutes)"):
-            aws_client.update_event_rule(name=eb_rule, rate=5)
-            return
-
-        if current_song and (current_rule_schedule == "rate(5 minutes)"):
-            aws_client.update_event_rule(name=eb_rule, rate=1)
-
+        reschedule(aws_client=aws_client, listening_now=(current_song is not None))
     except ClientError as ex:
         logger.log_failure(context.function_name, ex)
 
