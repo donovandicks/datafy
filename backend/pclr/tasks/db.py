5fe053bee04a741d6cd4299f1693ca7835293d08
Donovan Dicks <donovan.dicks@outlook.com>
2022-03-17 21:11:33 -0400

    Add existence checks
    Used to avoid conflicts on all tables

--- backend/pclr/tasks/db.py
+++ backend/pclr/tasks/db.py
@@ -1,22 +1,21 @@
 """Tasks related to interacting with the Database"""
-from typing import Union
-
 from clients.postgres import PostgresClient
+from domain.common import check_exists
 from models.db import FilterExpression, UpdateClause
 from models.track import Track
-from prefect.triggers import all_successful
-from prefect.utilities.tasks import task
-from prefect.utilities.context import context
 
-logger = context.get("logger")
+import prefect
+from prefect import task
+
+logger = prefect.logging.get_logger(__name__)
 
 
 @task
-def insert_album(client: PostgresClient, track: Union[Track, None]) -> bool:
+def insert_album(client: PostgresClient, track: Track) -> bool:
     """Inserts an album"""
-    if not track:
-        logger.info("No album to insert")
-        return False
+    album_exists = check_exists(client=client, track=track, item="album")
+    if album_exists:
+        return True
 
     logger.info(f"Inserting album {track.album_id}")
 
@@ -25,16 +24,21 @@ def insert_album(client: PostgresClient, track: Union[Track, None]) -> bool:
         track.album_name,
     )
 
-    client.insert(table="album", values=values)
+    try:
+        client.insert(table="album", values=values)
+    except Exception as ex:
+        logger.error(f"Failed to insert album {track.album_id}")
+        return False
+
     return True
 
 
 @task
-def insert_artist(client: PostgresClient, track: Union[Track, None]) -> bool:
+def insert_artist(client: PostgresClient, track: Track) -> bool:
     """Inserts an artist"""
-    if not track:
-        logger.info("No artist to insert")
-        return False
+    artist_exists = check_exists(client=client, track=track, item="artist")
+    if artist_exists:
+        return True
 
     logger.info(f"Inserting artist {track.artist_id}")
 
@@ -43,16 +47,21 @@ def insert_artist(client: PostgresClient, track: Union[Track, None]) -> bool:
         track.artist_name,
     )
 
-    client.insert(table="artist", values=values)
+    try:
+        client.insert(table="artist", values=values)
+    except Exception as ex:
+        logger.error(f"Failed to insert artist {track.artist_id}: {ex}")
+        return False
+
     return True
 
 
 @task
-def insert_track(client: PostgresClient, track: Union[Track, None]) -> bool:
+def insert_track(client: PostgresClient, track: Track) -> bool:
     """Inserts a track"""
-    if not track:
-        logger.info("No track to insert")
-        return False
+    track_exists = check_exists(client=client, track=track, item="track")
+    if track_exists:
+        return True
 
     logger.info(f"Inserting track {track.track_id}")
 
@@ -63,17 +72,18 @@ def insert_track(client: PostgresClient, track: Union[Track, None]) -> bool:
         track.album_id,
     )
 
-    client.insert(table="track", values=values)
+    try:
+        client.insert(table="track", values=values)
+    except Exception as ex:
+        logger.error(f"Failed insert track {track.track_id}: {ex}")
+        return False
+
     return True
 
 
 @task
-def count_new_track(client: PostgresClient, track: Union[Track, None]) -> bool:
+def count_new_track(client: PostgresClient, track: Track) -> bool:
     """Inserts a play count record"""
-    if not track:
-        logger.info("No track to count")
-        return False
-
     logger.info(f"Counting play for track {track.track_id}")
 
     values = (
@@ -87,13 +97,9 @@ def count_new_track(client: PostgresClient, track: Union[Track, None]) -> bool:
 
 
 @task
-def update_track_count(client: PostgresClient, track: Union[Track, None]) -> bool:
+def update_track_count(client: PostgresClient, track: Track) -> bool:
     """Updates the play count for an existing track"""
-    if not track:
-        logger.info("No track to update")
-        return False
-
-    logger.info(f"Updated play count for {track.track_id}")
+    logger.info(f"Updating play count for {track.track_id}")
 
     update = UpdateClause(
         update_field="total_play_count",
@@ -105,32 +111,23 @@ def update_track_count(client: PostgresClient, track: Union[Track, None]) -> boo
         ),
     )
 
-    client.update(table="play_count", clause=update)
+    try:
+        client.update(table="play_count", clause=update)
+    except Exception as ex:
+        logger.error(f"Failed to update play count for {track.track_id}: {ex}")
+        return False
+
     return True
 
 
 @task
-def check_exists(client: PostgresClient, track: Union[Track, None]) -> bool:
+def check_counted(client: PostgresClient, track: Track) -> bool:
     """Checks for an existing track"""
-    if not track:
-        logger.info("No track to search for")
-        return False
-
-    filter_expr = FilterExpression(
-        filter_field="track_id",
-        filter_condition="=",
-        filter_value=track.track_id,
-    )
-
-    result = client.get_row(table="play_count", filter_expr=filter_expr)
-    logger.info(
-        f"Retrieved database item where {filter_expr.build_filter_expression()}: {result}"
-    )
-
-    return bool(result)
+    logger.info(f"Checking if track {track.track_id} is being counted")
+    return check_exists(client=client, track=track, item="play_count")
 
 
-@task(trigger=all_successful)
+@task
 def view_database(client: PostgresClient):
     """Shows existing data in the db"""
     logger.info("Retrieving all data from the database")
