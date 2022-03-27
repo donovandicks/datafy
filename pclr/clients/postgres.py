3e5c49f51e4cb04042936c8f128932f56ea579b8
Donovan Dicks <donovan.dicks@outlook.com>
2022-03-27 00:30:15 -0400

    Add convenience type alias

--- pclr/clients/postgres.py
+++ pclr/clients/postgres.py
@@ -1,11 +1,13 @@
 """Code for connection to Postgresql"""
 
 import os
-from typing import Any, List, Type
+from typing import Any, List, Type, TypeAlias
 
 # Required to initialize schema
 import models.db  # pylint: disable=unused-import
 from sqlmodel import Session, SQLModel, create_engine, select
+
+Table: TypeAlias = Type[SQLModel]
 
 
 class PostgresClient:
@@ -26,7 +28,7 @@ class PostgresClient:
 
     def update(
         self,
-        table: Type[SQLModel],
+        table: Table,
         filter_key: str,
         filter_value: Any,
         update_key: str,
@@ -41,17 +43,17 @@ class PostgresClient:
             session.add(result)
             session.commit()
 
-    def get_all_rows(self, table: Type[SQLModel]) -> List:
+    def get_all_rows(self, table: Table) -> List:
         """Retrieves all rows from a table"""
         with Session(self.__engine) as session:
             return session.exec(select(table)).all()
 
-    def get_row(self, table: Type[SQLModel], key: str, value: Any) -> List:
+    def get_row(self, table: Table, key: str, value: Any) -> List:
         """Retrieves a single row where the conditions are met"""
         with Session(self.__engine) as session:
             stmt = select(table).where(key == value)
             return session.exec(stmt).all()
 
-    def check_exists(self, table: Type[SQLModel], key: str, value: Any) -> bool:
+    def check_exists(self, table: Table, key: str, value: Any) -> bool:
         """Checks if a row exists on a table where the given key matches the value"""
         return bool(self.get_row(table=table, key=key, value=value))
